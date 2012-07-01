e05a3a625a1cc7cfb7d94741671e9292c01efcd2
Joomlapro <contato@joomlapro.com.br>
2012-07-01 18:18:02 -0300

    Fixed phpcs errors

--- mod_comparison.php
+++ mod_comparison.php
@@ -1,12 +1,12 @@
 <?php
 /**
- * @package		Comparison
- * @subpackage	mod_comparison
- * @copyright	Copyright (C) AtomTech, Inc. All rights reserved.
- * @license		GNU General Public License version 2 or later; see LICENSE.txt
+ * @package     Comparison
+ * @subpackage  mod_comparison
+ * @copyright   Copyright (C) 2012 AtomTech, Inc. All rights reserved.
+ * @license     GNU General Public License version 2 or later; see LICENSE.txt
  */
 
-// no direct access
+// No direct access
 defined('_JEXEC') or die;
 
 $moduleclass_sfx = htmlspecialchars($params->get('moduleclass_sfx'));
--- tmpl/default.php
+++ tmpl/default.php
@@ -1,38 +1,38 @@
 <?php
 /**
- * @package		Comparison
- * @subpackage	mod_comparison
- * @copyright	Copyright (C) AtomTech, Inc. All rights reserved.
- * @license		GNU General Public License version 2 or later; see LICENSE.txt
+ * @package     Comparison
+ * @subpackage  mod_comparison
+ * @copyright   Copyright (C) 2012 AtomTech, Inc. All rights reserved.
+ * @license     GNU General Public License version 2 or later; see LICENSE.txt
  */
 
-// no direct access
+// No direct access
 defined('_JEXEC') or die;
 
 JHtml::stylesheet('mod_comparison/comparison.css', false, true, false);
 JHtml::script('mod_comparison/jquery.min.js', false, true);
 JHtml::script('mod_comparison/jquery.qbeforeafter.js', false, true);
 ?>
-<div class="comparisons<?php echo $moduleclass_sfx ?>">
+<div class="comparisons<?php echo $moduleclass_sfx; ?>">
 	<style type="text/css">
-		.comparison<?php echo $uniqueid ?> {
-			width: <?php echo $width ?>px;
-			height: <?php echo $height ?>px;
+		.comparison<?php echo $uniqueid; ?> {
+			width: <?php echo $width; ?>px;
+			height: <?php echo $height; ?>px;
 		}
 	</style>
 	<script type="text/javascript">
 		$(function () {
-		    $('.comparison<?php echo $uniqueid ?>').qbeforeafter({
-		        defaultgap: <?php echo intval($width / 2) ?>,
+		    $('.comparison<?php echo $uniqueid; ?>').qbeforeafter({
+		        defaultgap: <?php echo intval($width / 2); ?>,
 		        leftgap: 0,
 		        rightgap: 0,
-		        caption: <?php echo $captions ?>,
+		        caption: <?php echo $captions; ?>,
 		        reveal: 0.5
 		    });
 		});
 	</script>
 	<div class="comparison<?php echo $uniqueid ?>">
-		<img width="<?php echo $width ?>" height="<?php echo $height ?>" alt="<?php echo $beforecaption ?>" src="<?php echo 'images/' . $beforeimage ?>" />
-		<img width="<?php echo $width ?>" height="<?php echo $height ?>" alt="<?php echo $aftercaption ?>" src="<?php echo 'images/' .$afterimage ?>" />
+		<img width="<?php echo $width; ?>" height="<?php echo $height; ?>" alt="<?php echo $beforecaption; ?>" src="<?php echo 'images/' . $beforeimage; ?>" />
+		<img width="<?php echo $width; ?>" height="<?php echo $height; ?>" alt="<?php echo $aftercaption; ?>" src="<?php echo 'images/' . $afterimage; ?>" />
 	</div>
 </div>
